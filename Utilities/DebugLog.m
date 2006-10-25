df90eba7b92477a4b200f2493f9f21b191a4e01c
charles <charles@82e7c6bb-cafa-0310-8398-98fdc49d3415>
2006-10-25 05:21:41 +0000

    GridStuffer: simplify DebugLog, used in DEBUG builds to log messages to the console
    git-svn-id: file:///Users/charles/Docs/Dev+/Repository/GridStuffer/trunk@166 82e7c6bb-cafa-0310-8398-98fdc49d3415

--- Utilities/DebugLog.m
+++ Utilities/DebugLog.m
@@ -7,15 +7,6 @@
 
 #ifdef DEBUG
 
-static NSArray *identifierArray = nil;
-
-NSArray *identifiers ()
-{
-	if ( identifierArray == nil )
-		identifierArray = [[[NSUserDefaults standardUserDefaults] objectForKey:@"DebugIdentifiers"] retain];
-	return identifierArray;
-}
-
 //valid identifiers and verbose level may be set using the user defaults
 //if the value for one of the user defaults is nil, just ignore that setting
 void DLog(NSString *identifier, int level, NSString *fmt,...)
@@ -24,12 +15,7 @@ void DLog(NSString *identifier, int level, NSString *fmt,...)
 	id currentVerboseLevel = [[NSUserDefaults standardUserDefaults] valueForKey:@"DebugLogVerboseLevel"];
 	if ( currentVerboseLevel != nil && level > [currentVerboseLevel intValue] )
 		return;
-	
-	//check the identifer
-	NSArray *ids = identifiers();
-	if ( (identifier !=nil) && ( ids!=nil ) && ([ids indexOfObject:identifier] == NSNotFound) )
-		return;
-	
+		
 	//now, we can log!
     va_list ap;
     va_start(ap,fmt);
